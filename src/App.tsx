55587d36574373058f7a4b6742e189406d40948f
Martin Hubelbauer <joeysykes18@gmail.com>
2024-01-11 11:00:15 +0100

    Remove unused template code and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,17 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import { POSITIVE_TRAITS, NEUTRAL_TRAITS, NEGATIVE_TRAITS } from './constants';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
+      <h1>Generate Traits</h1>
       <div>
         {POSITIVE_TRAITS.length} positive,
         {NEUTRAL_TRAITS.length} neutral,
         {NEGATIVE_TRAITS.length} negative,
       </div>
 
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <button>Generate!</button>
     </>
   );
 }
